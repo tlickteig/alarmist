e8f2d7179b2e9ead6277fc2eec0ab534d82f6b78
Nova Hickie <ambreejock@gmail.com>
2024-03-01 18:59:56 -0600

    Add dark mode

--- Android/app/src/main/java/com/alarmist/Alarmist/MainActivity.kt
+++ Android/app/src/main/java/com/alarmist/Alarmist/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.alarmist.Alarmist
 
+import com.alarmist.Alarmist.classes.CustomColors
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import java.util.Date
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +18,39 @@ class MainActivity : ComponentActivity() {
 
         setContent { // In here, we can call composables!
             MaterialTheme {
-                Greeting(name = "gfhyf")
+                Row {
+                    Column {
+                        Greeting(name = "World")
+                        CurrentTime()
+                        GetAlarmName()
+                        GetAlarmTime()
+                    }
+                }
             }
         }
     }
 }
 
+
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+        Text(text = "Hello $name!", color = CustomColors.TextColor)
+    }
+}
+
+@Preview
+@Composable
+fun CurrentTime() {
+        Text(text = "When you opened the app it was "+ Date().toString(), color = CustomColors.TextColor)
+}
+
+@Composable
+fun GetAlarmName() {
+    Text(text = "Undefined Name.", color = CustomColors.TextColor)
+}
+
+@Composable
+fun GetAlarmTime() {
+    Text(text = Date().time.toString(), color = CustomColors.TextColor)
 }
---
+++ Android/app/src/main/java/com/alarmist/Alarmist/classes/CustomColors.kt
@@ -0,0 +1,26 @@
+package com.alarmist.Alarmist.classes
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+class CustomColors {
+
+    companion object {
+        val BackgroundColor: Color
+            @Composable
+            get() = if (isSystemInDarkTheme()) {
+                Color(0xFF4A4A4A)
+            } else {
+                Color(0xFFEDEDED)
+            }
+
+        val TextColor: Color
+            @Composable
+            get() = if (isSystemInDarkTheme()) {
+                Color(0xFFEAEAEA)
+            } else {
+                Color(0xFF4A4A4A)
+            }
+    }
+}
